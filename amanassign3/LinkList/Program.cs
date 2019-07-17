de05838881e4d707cb19fde3a5e1a599b610db73
Amandeep  Singh <740035@student.tor.loc>
2019-07-17 11:43:38 -0400

    final commit

---
+++ amanassign3/LinkList/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new LinkedList();
+            a.CreateLinkedList();
+        }
+    }
+
+    class LinkList
+    {
+        Node Head;
+    }
+
+    class Node
+    {
+        public string studentname;
+        public Node nextLink;
+        public Node prev;
+        String nodeName;
+
+
+    }
+
+    class LinkedList
+    {
+        Node Head, Orange, Tangerine, Tail;
+
+        public void CreateLinkedList()
+        {
+            Head = new Node();
+            Orange = new Node();
+            Tangerine = new Node();
+            Tail = Tangerine;
+
+            Head.nextLink = Orange;
+            Console.WriteLine("yeahh!!!!");
+        }
+
+    }
+}
